419b1be45fd7cec9e5fb9f9d59e3dcd7b3e011b2
Milos Zivlak <zivlakmilos@gmail.com>
2020-03-03 19:05:48 +0100

    Added licence header

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,31 @@
+/* 
+ * This file is part of the Middleware Board distribution (https://github.com/Elektropioneer/Middleware_Board).
+ * Copyright (c) 2020 Miloš Zivlak (milos.zivlak@sensa-group.net).
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+  pinMode(PC13, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  digitalWrite(PC13, HIGH);
+  delay(1000);
+  digitalWrite(PC13, LOW);
+  delay(1000);
 }
